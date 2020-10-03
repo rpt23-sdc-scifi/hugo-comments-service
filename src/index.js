8f87d5d8ea95b58cb4e429efdee5f5ed4f088bd2
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-10-03 12:34:16 -0700

    webpack, babel, react initial setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+        <div>Comments will be here.. eventually</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#comments"));
