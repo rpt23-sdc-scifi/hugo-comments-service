e312220002ba8b1b3132dbb63bcfc71c07ef643b
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-10-04 13:50:17 -0700

    change mode= based off Nicholas' PR feedback

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,3 @@
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader"
-//         }
-//       }
-//     ]
-//   }
-// };
-
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
