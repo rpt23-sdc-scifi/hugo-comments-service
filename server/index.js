97fda7991889192a62357814f45e8b7d76dbc0d5
Hugo Xiong <hugo@fluentkey.com>
2020-11-21 11:52:59 -0800

    edit linting

--- server/index.js
+++ server/index.js
@@ -1,79 +1,82 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const db = require('../db/index');
-const path = require('path');
-const expressStaticGzip = require('express-static-gzip');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const db = require("../db/index");
+const path = require("path");
+const expressStaticGzip = require("express-static-gzip");
 
 const port = 4000;
 
 const app = express();
 
 // app.use(express.static('./dist'));
-app.use('/', expressStaticGzip(('./dist'), {
-  enableBrotli: true,
-   orderPreference: ['br', 'gz'],
-   setHeaders: function (res, path) {
+app.use(
+  "/",
+  expressStaticGzip("./dist", {
+    enableBrotli: true,
+    orderPreference: ["br", "gz"],
+    setHeaders: function (res, path) {
       res.setHeader("Cache-Control", "dist, max-age=31536000");
-   }
-}))
+    },
+  })
+);
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/comments', async(req, res) => {
+app.get("/comments", async (req, res) => {
   try {
     const comments = await db.getComments();
     res.status(200).send({
       success: true,
       count: comments.length,
-      data: comments
+      data: comments,
     });
   } catch (error) {
     console.error(error);
     res.status(400).json({
       success: false,
-      msg: error
+      msg: error,
     });
   }
 });
 
-app.get('/comments/:id', async(req, res) => {
+app.get("/comments/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
     const comment = await db.getComment(id);
 
-    if ( !comment || id > 100 ) {
+    if (!comment || id > 100) {
       return res.status(400).json({
         succes: false,
-        msg: `no song with id ${id}`
+        msg: `no song with id ${id}`,
       });
     }
 
-    if (  comment.length === 0 ) {
+    if (comment.length === 0) {
       return res.status(400).json({
         succes: false,
-        msg: `song ${id} doesn't have comments`
+        msg: `song ${id} doesn't have comments`,
       });
     }
 
     res.status(200).send({
       success: true,
-      data: comment
+      data: comment,
     });
   } catch (error) {
     console.error(error);
     res.status(400).json({
       succes: false,
-      msg: error
+      msg: error,
     });
   }
 });
 
-app.get('/:current', (req, res) => {
-  console.log('hit');
-  res.sendFile(path.join(__dirname,'../dist/index.html'));
+app.get("/:current", (req, res) => {
+  console.log("hit");
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 app.listen(port, () => {
