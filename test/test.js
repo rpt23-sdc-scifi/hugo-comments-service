493f9da5cb6e543fe7e90311402abd9ae4360d6e
Hugo Xiong <hugo@fluentkey.com>
2020-11-28 19:54:29 -0800

    update tests with prettier

--- test/test.js
+++ test/test.js
@@ -1,70 +1,77 @@
-const mongoose = require('mongoose');
-const db = require('../db/index');
+const mongoose = require("mongoose");
+const db = require("../db/index");
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let app = require('../server/index');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let app = require("../server/index");
 
 let should = chai.should();
 chai.use(chaiHttp);
 
-describe('/GET comments', () => {
-  it('it should GET all the comments', (done) => {
-    chai.request(app)
-        .get('/comments')
-        .end((err, res) => {
-              res.should.have.status(200);
-              res.body.data.should.be.a('array');
-              res.body.data.length.should.be.gt(36);
-          done();
-        });
+describe("/GET comments", () => {
+  it("it should GET all the comments", (done) => {
+    chai
+      .request(app)
+      .get("/comments")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.data.should.be.a("array");
+        res.body.data.length.should.be.gt(36);
+        done();
+      });
   });
 });
 
-describe('/GET comments by song ID', () => {
-  it('it should GET comments for song_id 1', (done) => {
-    chai.request(app)
-        .get('/comments/song/1')
-        .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.a('array');
-          done();
-        });
+describe("/GET comments by song ID", () => {
+  it("it should GET comments for song_id 1", (done) => {
+    chai
+      .request(app)
+      .get("/comments/song/1")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        done();
+      });
   });
 
-  it('it should not GET any comments for a non-existant song_id 50000', (done) => {
-    chai.request(app)
-        .get('/comments/song/50000')
-        .end((err, res) => {
-              res.should.have.status(400);
-              console.log(res.body.error);
-              res.body.error.should.be.a('string');
-              res.body.error.should.equal("song 50000 doesn't have comments");
-          done();
-        });
+  it("it should not GET any comments for a non-existant song_id 50000", (done) => {
+    chai
+      .request(app)
+      .get("/comments/song/50000")
+      .end((err, res) => {
+        res.should.have.status(400);
+        console.log(res.body.error);
+        res.body.error.should.be.a("string");
+        res.body.error.should.equal("song 50000 doesn't have comments");
+        done();
+      });
   });
 });
 
-describe('/GET comments by comment ID', () => {
-  it('it should GET comments for comment_id 1', (done) => {
-    chai.request(app)
-        .get('/comments/id/1')
-        .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.a('object');
-          done();
-        });
+describe("/GET comments by comment ID", () => {
+  it("it should GET comments for comment_id 1", (done) => {
+    chai
+      .request(app)
+      .get("/comments/id/1")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        done();
+      });
   });
 
-  it('it should not GET any comments for a non-existant id 50000', (done) => {
-    chai.request(app)
-        .get('/comments/id/50000')
-        .end((err, res) => {
-              res.should.have.status(400);
-              console.log(res.body.error);
-              res.body.error.should.be.a('string');
-              res.body.error.should.equal("no comment with id 50000");
-          done();
-        });
+  it("it should not GET any comments for a non-existant id 50000", (done) => {
+    chai
+      .request(app)
+      .get("/comments/id/50000")
+      .end((err, res) => {
+        res.should.have.status(400);
+        console.log(res.body.error);
+        res.body.error.should.be.a("string");
+        res.body.error.should.equal("no comment with id 50000");
+        done();
+      });
   });
 });
+
+describe("");
