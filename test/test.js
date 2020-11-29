db1491e942b5d224457aceb93d6aad54eb88eb1c
Hugo Xiong <hugo@fluentkey.com>
2020-11-28 20:41:27 -0800

    add update comment test

--- test/test.js
+++ test/test.js
@@ -10,6 +10,25 @@ chai.use(chaiHttp);
 
 // I should probably be using a seperate test database for these tests, but I don't feel like coding it right now, maybe later
 
+// ObjectID of comment that exists
+let existingComment = {
+  _id: "5fc3223c5b11641d723798c8",
+  user_id: 4,
+  song_id: 29,
+  content: "Id anim dolor ea aliquip occaecat consectetur.",
+  time_stamp: 125,
+};
+
+let newComment = {
+  user_id: 45,
+  song_id: 182,
+  content: "WTF??? This song is terrible.",
+  time_stamp: 333,
+  random: true, // this should not be saved because it's not in the schema
+};
+
+let newCommentID = null;
+
 describe("/GET comments", () => {
   it("should GET all the comments", (done) => {
     chai
@@ -54,7 +73,7 @@ describe("/GET comments by comment ID", () => {
   it("should GET comments for specific comment_id", (done) => {
     chai
       .request(app)
-      .get("/comments/id/5fc3223c5b11641d723798c8")
+      .get(`/comments/id/${existingComment._id}`)
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a("object");
@@ -74,50 +93,40 @@ describe("/GET comments by comment ID", () => {
   });
 });
 
-describe("/POST create new comment", () => {
-  it("should create a new comment", (done) => {
-    chai
-      .request(app)
-      .post("/comments")
-      .type('json')
-      .send({
-        "user_id": 45,
-        "song_id": 182,
-        "content": "WTF??? This song is terrible.",
-        "time_stamp": 333,
-        "random": true // this should not be saved because it's not in the schema
-      })
-      .end((err, res) => {
-        res.should.have.status(201);
-        res.body.should.be.a("object");
-        res.body.song_id.should.equal(182);
-        res.body.user_id.should.equal(45);
-        res.body.should.not.have.property("random");
-        done();
-      })
-  })
-});
-
 describe("/POST comment", () => {
   it("should create a new comment", (done) => {
     chai
       .request(app)
       .post("/comments")
-      .type('json')
-      .send({
-        "user_id": 45,
-        "song_id": 182,
-        "content": "WTF??? This song is terrible.",
-        "time_stamp": 333,
-        "random": true // this should not be saved because it's not in the schema
-      })
+      .type("json")
+      .send(newComment)
       .end((err, res) => {
         res.should.have.status(201);
         res.body.should.be.a("object");
         res.body.song_id.should.equal(182);
-        res.body.user_id.should.equal(45);
+        res.body.content.should.equal("WTF??? This song is terrible.");
         res.body.should.not.have.property("random");
+        newCommentID = res.body._id;
         done();
+      });
+  });
+});
+
+describe("/PATCH comment", () => {
+  it("should update an existing comment", (done) => {
+    chai
+      .request(app)
+      .patch(`/comments/id/${newCommentID}`)
+      .type("json")
+      .send({
+        content: "Yo, this is the best song EVER!!!",
       })
-  })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.song_id.should.equal(182);
+        res.body.content.should.equal("Yo, this is the best song EVER!!!");
+        done();
+      });
+  });
 });
