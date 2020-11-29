0afc10e0256e606233e5c83622c2448fabda6d3a
Hugo Xiong <hugo@fluentkey.com>
2020-11-28 19:51:12 -0800

    add test for get comment by comment ID

--- test/test.js
+++ test/test.js
@@ -21,7 +21,7 @@ describe('/GET comments', () => {
   });
 });
 
-describe('/GET comments', () => {
+describe('/GET comments by song ID', () => {
   it('it should GET comments for song_id 1', (done) => {
     chai.request(app)
         .get('/comments/song/1')
@@ -40,6 +40,30 @@ describe('/GET comments', () => {
               console.log(res.body.error);
               res.body.error.should.be.a('string');
               res.body.error.should.equal("song 50000 doesn't have comments");
+          done();
+        });
+  });
+});
+
+describe('/GET comments by comment ID', () => {
+  it('it should GET comments for comment_id 1', (done) => {
+    chai.request(app)
+        .get('/comments/id/1')
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+          done();
+        });
+  });
+
+  it('it should not GET any comments for a non-existant id 50000', (done) => {
+    chai.request(app)
+        .get('/comments/id/50000')
+        .end((err, res) => {
+              res.should.have.status(400);
+              console.log(res.body.error);
+              res.body.error.should.be.a('string');
+              res.body.error.should.equal("no comment with id 50000");
           done();
         });
   });
