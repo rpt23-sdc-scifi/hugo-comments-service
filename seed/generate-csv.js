81eaf57100f8e14667e10e5e69e21511b4aa6305
Hugo Xiong <hugo@fluentkey.com>
2020-12-05 14:30:19 -0800

    csv tables script updated

--- seed/generate-csv.js
+++ seed/generate-csv.js
@@ -2,7 +2,14 @@ const fs = require("fs");
 const csvWriter = require("csv-write-stream");
 const loremIpsum = require("lorem-ipsum").LoremIpsum;
 
-const count = 5; // 6 / 10 million comments
+// In primary "comments" table, generate 100 million records
+// In referenced "songs" table, generate 10 million records
+// In referenced "users" table, generate 10 million users
+// In referenced "content" table, generate 100 million records
+
+// Each table corresponds to a CSV file
+
+const count = 10; // number of comments
 const maxSongLength = 480; // in seconds
 
 const lorem = new loremIpsum({
@@ -13,30 +20,61 @@ const lorem = new loremIpsum({
 });
 
 const getRandomUserId = () => {
-  return Math.floor(Math.random() * 1000000) + 1; // 1 million users
+  return Math.floor(Math.random() * 10000000) + 1; // 10 million users
 };
 
 const getRandomSongId = () => {
-  return Math.floor(Math.random() * 1000000) + 1; // 1 million songs
+  return Math.floor(Math.random() * 10000000) + 1; // 10 million songs
+};
+
+const getRandomContentId = () => {
+  return Math.floor(Math.random() * 10000000) + 1; // 100 million text comments
 };
 
 const getRandomTimeStamp = (maxTime) => {
   return Math.floor(Math.random() * maxTime);
 };
 
-const writer = csvWriter();
+const commentsWriter = csvWriter();
+const usersWriter = csvWriter();
+const songsWriter = csvWriter();
+const contentWriter = csvWriter();
 
 // adding the option {flags: "a"} means it will append instead of overwriting it; "w" is the default overwrite
-writer.pipe(fs.createWriteStream("./seed/demo.csv", {flags: "a"}));
+commentsWriter.pipe(fs.createWriteStream("./seed/comments.csv"));
+songsWriter.pipe(fs.createWriteStream("./seed/songs.csv"));
+usersWriter.pipe(fs.createWriteStream("./seed/users.csv"));
+contentWriter.pipe(fs.createWriteStream("./seed/content.csv"));
 
 for (let i = 1; i <= count; i++) {
-  const comment = {
-    user_id: getRandomUserId(),
-    song_id: getRandomSongId(),
-    content: lorem.generateSentences(1),
-    time_stamp: getRandomTimeStamp(maxSongLength),
-  };
-  writer.write(comment);
+  const user_id = getRandomUserId();
+  const song_id = getRandomSongId();
+  const content_id = getRandomContentId();
+  const text = lorem.generateSentences(1);
+  const time_stamp = getRandomTimeStamp(maxSongLength);
+
+  commentsWriter.write({
+    user_id,
+    song_id,
+    content_id,
+    time_stamp,
+  });
+
+  usersWriter.write({
+    system_number: user_id,
+  });
+
+  songsWriter.write({
+    system_number: song_id,
+  });
+
+
+  contentWriter.write({
+    text,
+  });
 }
 
-writer.end();
+commentsWriter.end();
+usersWriter.end();
+songsWriter.end();
+contentWriter.end();
