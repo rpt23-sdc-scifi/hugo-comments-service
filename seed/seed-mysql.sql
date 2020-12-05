8e1200971883520478f6b56014fa02b9f6b48881
Hugo Xiong <hugo@fluentkey.com>
2020-12-05 14:03:11 -0800

    update sql script for more tables

--- seed/seed-mysql.sql
+++ seed/seed-mysql.sql
@@ -1,32 +1,110 @@
--- commands to reset database if necessary
--- DROP DATABASE IF EXISTS soundcloud;
--- CREATE DATABASE soundcloud;
--- USE soundcloud;
+-- script created from MySQL Workbench
 
-CREATE DATABASE IF NOT EXISTS soundcloud;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-USE soundcloud;
+-- -----------------------------------------------------
+-- Schema test_db_comments
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema soundcloud
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `soundcloud` ;
 
--- DEFINE SCHEMA
+-- -----------------------------------------------------
+-- Schema soundcloud
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `soundcloud` DEFAULT CHARACTER SET utf8 ;
+USE `soundcloud` ;
 
-CREATE TABLE IF NOT EXISTS comments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  song_id INT NOT NULL,
-  content VARCHAR(255) NOT NULL,
-  time_stamp INT NOT NULL
-);
+-- -----------------------------------------------------
+-- Table `soundcloud`.`songs`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `soundcloud`.`songs` ;
 
--- LOAD CSV DATA FROM FILE
+CREATE TABLE IF NOT EXISTS `soundcloud`.`songs` (
+  `song_id` INT NOT NULL AUTO_INCREMENT,
+  `system_number` INT NULL,
+  PRIMARY KEY (`song_id`))
+ENGINE = InnoDB;
 
-LOAD DATA LOCAL INFILE './seed/data.csv'
-INTO TABLE comments
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(user_id, song_id, content, time_stamp);
+CREATE INDEX `idx_system_number` ON `soundcloud`.`songs` (`system_number` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `soundcloud`.`users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `soundcloud`.`users` ;
+
+CREATE TABLE IF NOT EXISTS `soundcloud`.`users` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `system_number` INT NULL,
+  PRIMARY KEY (`user_id`))
+ENGINE = InnoDB;
+
+CREATE INDEX `idx_system_number` ON `soundcloud`.`users` (`system_number` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `soundcloud`.`content`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `soundcloud`.`content` ;
+
+CREATE TABLE IF NOT EXISTS `soundcloud`.`content` (
+  `content-Id` INT NOT NULL AUTO_INCREMENT,
+  `text` VARCHAR(255) NULL,
+  PRIMARY KEY (`content-Id`))
+ENGINE = InnoDB;
+
+CREATE INDEX `idx_text` ON `soundcloud`.`content` (`text` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `soundcloud`.`comments`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `soundcloud`.`comments` ;
+
+CREATE TABLE IF NOT EXISTS `soundcloud`.`comments` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `song_id` INT(11) NOT NULL,
+  `content_id` INT NOT NULL,
+  `time_stamp` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `song_id`
+    FOREIGN KEY (`song_id`)
+    REFERENCES `soundcloud`.`songs` (`song_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `soundcloud`.`users` (`user_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `content_id`
+    FOREIGN KEY (`content_id`)
+    REFERENCES `soundcloud`.`content` (`content-Id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 10259775
+DEFAULT CHARACTER SET = utf8;
+
+CREATE INDEX `idx_user_id` ON `soundcloud`.`comments` (`user_id` ASC);
+
+CREATE INDEX `idx_song_id` ON `soundcloud`.`comments` (`song_id` ASC);
+
+CREATE INDEX `idx_content` ON `soundcloud`.`comments` (`content_id` ASC);
+
+CREATE INDEX `idx_time_stamp` ON `soundcloud`.`comments` (`time_stamp` ASC);
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < seed/seed.sql
+ *    mysql -u root < seed/seed-mysql.sql
  *  to create the database and the tables.*/
