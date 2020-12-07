f7fe77a89c494048766148a1c55b92f2bbd361f8
Hugo Xiong <hugo@fluentkey.com>
2020-12-06 22:27:17 -0800

    fix get comment by ID bug

--- db/controller.js
+++ db/controller.js
@@ -2,6 +2,7 @@ const sequelize = require("./connect.js");
 const { User, Song, Content, Comment } = require("./models.js");
 
 const getComments = async (user_id, song_id, content) => {
+  // set up where conditions: IF the search filter is parameter, include it
   const userWhereCondition = user_id ? { system_number: user_id } : {};
   const songWhereCondition = song_id ? { system_number: song_id } : {};
   const contentWhereCondition = content ? { text: content } : {};
@@ -39,56 +40,120 @@ const getComments = async (user_id, song_id, content) => {
 };
 
 const getCommentByID = async (id) => {
-  const result = await Comment.findByPk(id);
+  const result = await Comment.findByPk(id, {
+    raw: true,
+    limit: 1000,
+    attributes: [
+      "comment_id",
+      [sequelize.col("User.system_number"), "user_id"],
+      [sequelize.col("Song.system_number"), "song_id"],
+      [sequelize.col("Content.text"), "content"],
+      "time_stamp",
+    ],
+    include: [
+      {
+        model: User,
+        attributes: [],
+      },
+      {
+        model: Song,
+        attributes: [],
+      },
+      {
+        model: Content,
+        attributes: [],
+
+      },
+    ],
+  });
+
   return result;
 };
 
-const findOrCreateReferenceData = async (data) => {
-  const user = await User.findOrCreate({
-    where: {
-      system_number: data.user_id,
-    },
-  });
-  const user_id = user[0].toJSON().user_id;
+const findOrCreateReferenceData = async (model, key, value, id) => {
+  const whereCondition = {};
+  whereCondition[key] = value;
 
-  const song = await Song.findOrCreate({
-    where: {
-      system_number: data.song_id,
-    },
-  });
-  const song_id = song[0].toJSON().song_id;
+  console.log("whereCondition:", whereCondition);
 
-  const content = await Content.findOrCreate({
-    where: {
-      text: data.content,
-    },
+  const instance = await model.findOrCreate({
+    where: whereCondition,
   });
-  const content_id = content[0].toJSON().content_id;
 
-  return {
-    song_id,
-    user_id,
-    content_id,
-    time_stamp: data.time_stamp,
-  };
+  const result = instance[0].toJSON()[id];
+
+  return result;
 };
 
-const saveComment = async (comment) => {
-  // get user, song, content info -- create if doesn't exist
-  const data = await findOrCreateReferenceData(comment);
+const saveComment = async (data) => {
+  // get user, song, content data -- create if doesn't exist, [+ time data]
+
+  const { user_id, song_id, content } = data;
+
+  data.user_id = await findOrCreateReferenceData(
+    User,
+    "system_number",
+    user_id,
+    "user_id"
+  );
+
+  data.song_id = await findOrCreateReferenceData(
+    Song,
+    "system_number",
+    song_id,
+    "song_id"
+  );
+
+  data.content_id = await findOrCreateReferenceData(
+    Content,
+    "text",
+    content,
+    "content_id"
+  );
+
   // create new comment with keys pointing to referenced tables
   const newComment = await Comment.create(data);
   return newComment;
 };
 
 const updateComment = async (id, data) => {
-  const result = await Comment.findOneAndUpdate({ _id: id }, data, {
-    new: true,
-  });
-  if (result === null) {
-    throw new Error(`no comment with id ${id}`);
+  const { user_id, song_id, content, time_stamp } = data;
+
+  const comment = await Comment.findByPk(id);
+
+  if (user_id) {
+    comment.user_id = await findOrCreateReferenceData(
+      User,
+      "system_number",
+      user_id,
+      "user_id"
+    );
   }
-  return result;
+
+  if (song_id) {
+    comment.song_id = await findOrCreateReferenceData(
+      Song,
+      "system_number",
+      song_id,
+      "song_id"
+    );
+  }
+
+  if (content) {
+    comment.content_id = await findOrCreateReferenceData(
+      Content,
+      "text",
+      content,
+      "content_id"
+    );
+  }
+
+  comment.time_stamp = time_stamp ? time_stamp : comment.time_stamp;
+
+  await comment.save();
+  await comment.reload();
+
+  return comment;
 };
 
 const deleteComment = async (id) => {
