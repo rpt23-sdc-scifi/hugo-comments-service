eef4e14f2213540d0d1d591f919effabf86fa187
Hugo Xiong <hugo@fluentkey.com>
2021-01-02 15:58:59 -0800

    add post stress test

--- test/k6_api_get.js
+++ test/k6_api_get.js
@@ -3,25 +3,24 @@ import http from "k6/http";
 import { Rate } from "k6/metrics";
 
 const myFailRate = new Rate("failed requests");
-const maxSongLength = 480; // in seconds
 
 export let options = {
   discardResponseBodies: true,
   scenarios: {
     rps_100: {
       executor: "constant-arrival-rate",
-      exec: "postCommentsApi",
+      exec: "getCommentsApi",
       rate: 100,
-      timeUnit: "1s",
+      timeUnit: "4s",
       duration: "30s",
       preAllocatedVUs: 100,
       maxVUs: 200,
     },
     rps_500: {
       executor: "constant-arrival-rate",
-      exec: "postCommentsApi",
+      exec: "getCommentsApi",
       rate: 500,
-      timeUnit: "1s",
+      timeUnit: "4s",
       duration: "30s",
       startTime: "30s",
       preAllocatedVUs: 500,
@@ -29,9 +28,9 @@ export let options = {
     },
     rps_750: {
       executor: "constant-arrival-rate",
-      exec: "postCommentsApi",
+      exec: "getCommentsApi",
       rate: 750,
-      timeUnit: "1s",
+      timeUnit: "4s",
       duration: "30s",
       startTime: "1m",
       preAllocatedVUs: 1000,
@@ -39,9 +38,9 @@ export let options = {
     },
     rps_1000: {
       executor: "constant-arrival-rate",
-      exec: "postCommentsApi",
+      exec: "getCommentsApi",
       rate: 1000,
-      timeUnit: "1s",
+      timeUnit: "4s",
       duration: "30s",
       startTime: "1m30s",
       preAllocatedVUs: 1500,
@@ -49,9 +48,9 @@ export let options = {
     },
     rps_1500: {
       executor: "constant-arrival-rate",
-      exec: "postCommentsApi",
+      exec: "getCommentsApi",
       rate: 1500,
-      timeUnit: "1s",
+      timeUnit: "4s",
       duration: "30s",
       startTime: "2m",
       preAllocatedVUs: 2000,
@@ -62,7 +61,7 @@ export let options = {
     // threshold (custom metric): 2% or less of requests return a 400 response (error / invalid request)
     "failed requests": [{ threshold: "rate<0.02", abortOnFail: true }],
     // threshold broken up by scenarios: 90% of requests must finish within 500ms, 95% within 800, and 99.9% within 1.5s.
-    "http_req_duration{scenario:rps_100}": [
+    'http_req_duration{scenario:rps_100}': [
       {
         threshold: "p(90) < 500",
         abortOnFail: true,
@@ -80,7 +79,7 @@ export let options = {
         abortOnFail: true,
       },
     ],
-    "http_req_duration{scenario:rps_500}": [
+    'http_req_duration{scenario:rps_500}': [
       {
         threshold: "p(90) < 500",
         abortOnFail: true,
@@ -98,7 +97,7 @@ export let options = {
         abortOnFail: true,
       },
     ],
-    "http_req_duration{scenario:rps_750}": [
+    'http_req_duration{scenario:rps_750}': [
       {
         threshold: "p(90) < 500",
         abortOnFail: true,
@@ -116,7 +115,7 @@ export let options = {
         abortOnFail: true,
       },
     ],
-    "http_req_duration{scenario:rps_1000}": [
+    'http_req_duration{scenario:rps_1000}': [
       {
         threshold: "p(90) < 500",
         abortOnFail: true,
@@ -134,7 +133,7 @@ export let options = {
         abortOnFail: true,
       },
     ],
-    "http_req_duration{scenario:rps_1500}": [
+    'http_req_duration{scenario:rps_1500}': [
       {
         threshold: "p(90) < 500",
         abortOnFail: true,
@@ -155,16 +154,28 @@ export let options = {
   },
 };
 
-export function postCommentsApi() {
-  const url = "http://localhost:4000/api/comments";
-  let headers = { "Content-Type": "application/json" };
-  let data = {
-    user_id: Math.ceil(Math.random() * 10000000),
-    song_id: Math.ceil(Math.random() * 10000000),
-    content: "k6 placeholder text",
-    time_stamp: Math.floor(Math.random() * maxSongLength),
-  };
+export function getCommentsApi() {
+  const commentId = Math.ceil(Math.random() * 100000000);
+  const resCommentQuery = http.get(
+    `http://localhost:4000/api/comments/${commentId}`
+  );
+  myFailRate.add(resCommentQuery.status !== 200);
 
-  let res = http.post(url, JSON.stringify(data), { headers: headers });
-  myFailRate.add(res.status !== 201);
+  const userId = Math.ceil(Math.random() * 10000000);
+  const resUserQuery = http.get(
+    `http://localhost:4000/api/comments?user_id=${userId}`
+  );
+  myFailRate.add(resUserQuery.status !== 200 && resUserQuery.status !== 404);
+
+  const songId = Math.ceil(Math.random() * 10000000);
+  const resSongQuery = http.get(
+    `http://localhost:4000/api/comments?song_id=${songId}`
+  );
+  myFailRate.add(resSongQuery.status !== 200 && resSongQuery.status !== 404);
+
+  const content = encodeURIComponent("Lorem ad aliquip et minim.");
+  const resContentQuery = http.get(
+    `http://localhost:4000/api/comments?content=${content}`
+  );
+  myFailRate.add(resContentQuery.status !== 200);
 }
