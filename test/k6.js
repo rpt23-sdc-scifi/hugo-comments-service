6a1940e216f7cd6cd15f67ecd52d410aafb3646e
Hugo Xiong <hugo@fluentkey.com>
2021-01-02 15:03:21 -0800

    add abort condition to thresholds

--- test/k6.js
+++ test/k6.js
@@ -6,42 +6,91 @@ import { Trend, Rate } from "k6/metrics";
 const myFailRate = new Rate("failed requests");
 
 export let options = {
+  discardResponseBodies: true,
   scenarios: {
-    constant_request_rate: {
-      executor: 'constant-arrival-rate',
-      rate: 400,
-      timeUnit: '4s',
-      duration: '10s',
+    rps_100: {
+      executor: "constant-arrival-rate",
+      exec: "getCommentsApi",
+      rate: 100,
+      timeUnit: "4s",
+      duration: "30s",
+      preAllocatedVUs: 100,
+      maxVUs: 200,
+    },
+    rps_500: {
+      executor: "constant-arrival-rate",
+      exec: "getCommentsApi",
+      rate: 500,
+      timeUnit: "4s",
+      duration: "30s",
+      startTime: "30s",
+      preAllocatedVUs: 500,
+      maxVUs: 1000,
+    },
+    rps_750: {
+      executor: "constant-arrival-rate",
+      exec: "getCommentsApi",
+      rate: 750,
+      timeUnit: "4s",
+      duration: "30s",
+      startTime: "1m",
       preAllocatedVUs: 1000,
       maxVUs: 2000,
     },
+    rps_1000: {
+      executor: "constant-arrival-rate",
+      exec: "getCommentsApi",
+      rate: 1000,
+      timeUnit: "4s",
+      duration: "30s",
+      startTime: "1m30s",
+      preAllocatedVUs: 1500,
+      maxVUs: 3000,
+    },
+    rps_1500: {
+      executor: "constant-arrival-rate",
+      exec: "getCommentsApi",
+      rate: 1500,
+      timeUnit: "4s",
+      duration: "30s",
+      startTime: "2m",
+      preAllocatedVUs: 2000,
+      maxVUs: 3000,
+    },
   },
-  // stages: [
-  //   { duration: '30s', target: 100 },
-  //   { duration: '30s', target: 100 },
-  //   { duration: '30s', target: 500 },
-  //   { duration: '30s', target: 500 }, // ~500 rps, around the breaking point
-  //   { duration: '30s', target: 800 },
-  //   { duration: '30s', target: 800 }, // ~570 rps, beyond the breaking point (maybe???)
-  //   { duration: '30s', target: 1000 },
-  //   { duration: '30s', target: 1000 },
-  //   { duration: '30s', target: 1200 },
-  //   { duration: '30s', target: 1200 },
-  // ],
   thresholds: {
     // threshold on a custom metric: 2% or less of requests return a 400 response (error / invalid request)
-    "failed requests": ["rate<0.02"],
+    "failed requests": [{ threshold: "rate<0.02", abortOnFail: true }],
     // threshold on standard metric: 90% of requests must finish within 500ms, 95% within 800, and 99.9% within 1.5s.
     http_req_duration: [
-      "p(90) < 500",
-      "p(95) < 800",
-      "p(99.9) < 2000",
-      "avg < 700",
+      {
+        threshold: "p(90) < 500",
+        abortOnFail: true,
+      },
+      {
+        threshold: "p(95) < 800",
+        abortOnFail: true,
+      },
+      {
+        threshold: "p(99.9) < 2000",
+        abortOnFail: true,
+      },
+      {
+        threshold: "avg < 700",
+        abortOnFail: true,
+      },
     ],
   },
 };
 
-export default function () {
+// threshold: [
+//   "p(90) < 500",
+//   "p(95) < 800",
+//   "p(99.9) < 2000",
+//   "avg < 700",
+// ],
+
+export function getCommentsApi() {
   const commentId = Math.ceil(Math.random() * 100000000);
   const resCommentQuery = http.get(
     `http://localhost:4000/api/comments/${commentId}`
@@ -65,5 +114,4 @@ export default function () {
     `http://localhost:4000/api/comments?content=${content}`
   );
   myFailRate.add(resContentQuery.status !== 200);
-
 }
