6038276ad17ad4d949b583530522f356d953b260
Hugo Xiong <hugo@fluentkey.com>
2021-01-02 13:03:39 -0800

    add scenarios with constant request rate

--- test/k6.js
+++ test/k6.js
@@ -6,18 +6,28 @@ import { Trend, Rate } from "k6/metrics";
 const myFailRate = new Rate("failed requests");
 
 export let options = {
-  stages: [
-    { duration: '30s', target: 100 },
-    { duration: '30s', target: 100 },
-    { duration: '30s', target: 500 },
-    { duration: '30s', target: 500 }, // ~500 rps, around the breaking point
-    { duration: '30s', target: 800 },
-    { duration: '30s', target: 800 }, // ~570 rps, beyond the breaking point (maybe???)
-    { duration: '30s', target: 1000 },
-    { duration: '30s', target: 1000 },
-    { duration: '30s', target: 1200 },
-    { duration: '30s', target: 1200 },
-  ],
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 400,
+      timeUnit: '4s',
+      duration: '10s',
+      preAllocatedVUs: 1000,
+      maxVUs: 2000,
+    },
+  },
+  // stages: [
+  //   { duration: '30s', target: 100 },
+  //   { duration: '30s', target: 100 },
+  //   { duration: '30s', target: 500 },
+  //   { duration: '30s', target: 500 }, // ~500 rps, around the breaking point
+  //   { duration: '30s', target: 800 },
+  //   { duration: '30s', target: 800 }, // ~570 rps, beyond the breaking point (maybe???)
+  //   { duration: '30s', target: 1000 },
+  //   { duration: '30s', target: 1000 },
+  //   { duration: '30s', target: 1200 },
+  //   { duration: '30s', target: 1200 },
+  // ],
   thresholds: {
     // threshold on a custom metric: 2% or less of requests return a 400 response (error / invalid request)
     "failed requests": ["rate<0.02"],
@@ -37,27 +47,23 @@ export default function () {
     `http://localhost:4000/api/comments/${commentId}`
   );
   myFailRate.add(resCommentQuery.status !== 200);
-  sleep(1);
 
   const userId = Math.floor(Math.random() * 10000000) + 1;
   const resUserQuery = http.get(
     `http://localhost:4000/api/comments?user_id=${userId}`
   );
   myFailRate.add(resUserQuery.status !== 200 && resUserQuery.status !== 404);
-  sleep(1);
 
   const songId = Math.ceil(Math.random() * 10000000);
   const resSongQuery = http.get(
     `http://localhost:4000/api/comments?song_id=${songId}`
   );
   myFailRate.add(resSongQuery.status !== 200 && resSongQuery.status !== 404);
-  sleep(1);
 
   const content = encodeURIComponent("Lorem ad aliquip et minim.");
   const resContentQuery = http.get(
     `http://localhost:4000/api/comments?content=${content}`
   );
   myFailRate.add(resContentQuery.status !== 200);
-  sleep(1);
 
 }
