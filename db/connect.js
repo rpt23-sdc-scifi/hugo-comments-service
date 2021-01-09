61202ae1d23ba0db5c2d68ce970bfd8fc8a05bfc
Hugo Xiong <hugo@fluentkey.com>
2021-01-08 16:16:21 -0800

    update connect syntax

--- db/connect.js
+++ db/connect.js
@@ -1,22 +1,22 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
 
-const sequelize = new Sequelize('soundcloud', user, password, {
+const sequelize = new Sequelize("soundcloud", user, password, {
   host: host,
-  dialect: 'mysql'
+  dialect: "mysql",
 });
 
 const verifyConnection = async () => {
   try {
     await sequelize.authenticate();
-    console.log('Sequelize connection has been established successfully.');
+    console.log("Sequelize connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the MySQL database:', error);
+    console.error("Unable to connect to the MySQL database:", error);
   }
-}
+};
 
 verifyConnection();
 
